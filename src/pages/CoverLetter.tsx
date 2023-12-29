2d42283ed60e47ac016a9a6c960b197bc3d39553
maxLiu <neoliukai@gmail.com>
2023-12-29 16:05:55 +0800

    feat: add react-copy-to-clipboard package and use it in CoverLetter.tsx
    This commit adds the react-copy-to-clipboard package as a dependency
    and uses it to copy the cover letter content to the clipboard when
    the user clicks the button. It also adds some environment variables
    to store the user's name, mobile and email, and uses them to generate
    the cover letter content. It also updates the .gitignore file to
    ignore the .env file. (main)

--- src/pages/CoverLetter.tsx
+++ src/pages/CoverLetter.tsx
@@ -1,15 +1,31 @@
 import { useImmer } from "use-immer";
 import { Box, Button, TextField, Typography } from "@mui/material";
+import { useEffect } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+type Data = {
+	companyName: string;
+	jobTitle: string;
+	content: string;
+	copied: boolean;
+};
 
 const CoverLetter = () => {
-	const [data, setData] = useImmer({
+	const myName = process.env.REACT_APP_MY_NAME;
+	const mobile = process.env.REACT_APP_MOBILE;
+	const email = process.env.REACT_APP_EMAIL;
+	const [data, setData] = useImmer<Data>({
 		companyName: "CoverLetter",
 		jobTitle: "Frontend Developer",
+		content: "",
+		copied: false,
 	});
-	const content = `
+
+	useEffect(() => {
+		const content = `
 尊敬的招聘主管：
 
-您好！我是劉凱銘，非常興奮有機會應徵${data.companyName}
+您好！我是${myName}，非常興奮有機會應徵${data.companyName}
 的${data.jobTitle}職位。透過我的經驗和技能，我相信我能夠為貴公司的團隊帶來價值。
 
 在過去的幾年中，我在前端開發領域積累了豐富的經驗。我精通於使用React.js和Angular.js等主流框架，並且擁有堅實的HTML、CSS和JavaScript基礎。我曾參與並主導過多個成功的專案，這些專案涉及使用者介面的設計、開發和優化。我熟悉現代前端工具和技術，如Webpack、Babel等，以確保項目的高效建置和部署。
@@ -21,13 +37,17 @@ const CoverLetter = () => {
 期待有機會與您進一步交流，進一步討論我如何可以成為${data.companyName}的有價值成員。謝謝您撥冗閱讀我的申請，期待能夠有機會與您共事。
 
 誠摯的，
-劉凱銘
-0916166023
-neoliukai@outlook.com`;
+${myName}
+${mobile}
+${email}`;
+		setData((draft) => {
+			draft.content = content;
+		});
+	}, [data]);
 
 	return (
 		<div>
-			<h1>Cover Letter</h1>
+			<Typography variant={"h1"}>Cover Letter</Typography>
 			<TextField
 				label="Job Title"
 				value={data.jobTitle}
@@ -50,14 +70,22 @@ neoliukai@outlook.com`;
 				component={"div"}
 				sx={{ p: 1, maxWidth: "500px", margin: "auto" }}
 			>
-				<Typography variant={"body1"}>{content}</Typography>
+				<Typography variant={"body1"}>{data.content}</Typography>
 			</Box>
-			<Button
+			<CopyToClipboard
+				text={data.content}
+				onCopy={()=>{
+					setData((draft) => {
+						draft.copied = true;
+					});
+				}}
+			>
+				<Button
 				variant="outlined"
-				onClick={() => navigator.clipboard.writeText(content)}
 			>
 				Copy Content
-			</Button>
+			</Button></CopyToClipboard>
+			{data.copied ? <span style={{ color: "red", padding: "0.25rem 1rem" }}>Copied.</span> : null}
 		</div>
 	);
 };
